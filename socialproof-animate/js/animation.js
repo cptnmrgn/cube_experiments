204c1ec68ab5eb7cdc1b5c92e2075fd3ebfc4367
cptnmrgn <cptnmrgn10@gmail.com>
2014-03-26 20:44:48 +1100

    added some animations

---
+++ socialproof-animate/js/animation.js
@@ -0,0 +1,34 @@
+var wobble = function() {
+  $("#tweet").removeClass('slideOutLeft');
+  $('#tweet').addClass('animated wobble');
+  // console.log("I'm called");
+}
+
+var move = function() {
+  $("#tweet").addClass('animated slideOutLeft');
+   setTimeout(wobble, 400);
+   
+}
+var initTwit= function() {
+  console.log('initiated!');
+  $(this).addClass('tweet');
+  setTimeout(move, 1000);
+}
+
+
+var instaWobble = function() {
+  $("#insta").removeClass('slideOutLeft');
+  $('#insta').addClass('animated wobble');
+}
+var instaMove = function() {
+  $("#insta").addClass('animated slideOutLeft');
+  setTimeout(instaWobble, 300);
+}
+var initInsta = function() {
+  console.log('initiating insta!');
+  $("#white").addClass('flash').fadeOut(2000);
+  setTimeout(instaMove, 1700);
+}
+
+$("#tweet").on('click', initTwit);
+$("#insta").on('click', initInsta);
