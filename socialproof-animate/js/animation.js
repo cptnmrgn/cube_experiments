2399bb8f9bf3f44c0ce2cfc25c8338058b2b9f39
cptnmrgn <cptnmrgn10@gmail.com>
2014-03-27 11:24:05 +1100

    centered feed div expansion animation. added notes to tweet animation

--- socialproof-animate/js/animation.js
+++ socialproof-animate/js/animation.js
@@ -1,27 +1,58 @@
 var wobble = function() {
-  $("#tweet").removeClass('slideOutLeft');
-  $('#tweet').addClass('animated wobble');
-  $('.feed').removeClass('hide');
-  $('.feed').animate({height:'85%'}, 300);
+  $("#bird").removeClass('slideOutLeft');
+  $('#bird').addClass('animated wobble');
+  var f = $('.feed');
+  f.removeClass('hide');
+  f.animate({left:'23%'}, {duration:300, queue: false});
+  f.animate({width:'74%'}, {duration:300, queue: false});
+  f.animate({bottom:'93%'}, {duration:300, queue: false});
+  f.animate({height:'85%'}, {duration:300, queue: false});
 }
 
 var move = function() {
-  $("#tweet").addClass('animated slideOutLeft');
+  $("#bird").addClass('animated slideOutLeft');
    setTimeout(wobble, 400);
    
 }
+
+var note1 = function() {
+  var m = $("#note1");
+  m.removeClass('hide');
+  m.animate({left:'57%'}, {duration:500, queue: false});
+  m.animate({bottom:'97%'}, {duration:500, queue: false});
+  m.delay(500).fadeOut(200);
+}
+
+var note2 = function() {
+  var m = $("#note2");
+  m.removeClass('hide');
+  m.animate({left:'57%'}, {duration:500, queue: false});
+  m.animate({bottom:'97%'}, {duration:500, queue: false});
+  m.delay(500).fadeOut(200);
+}
+
+var music = function() {
+  note1();
+  setTimeout(note2, 200);
+}
+
 var initTwit= function() {
   console.log('initiated!');
   $(this).addClass('tweet');
-  setTimeout(move, 1000);
+  setTimeout(music, 300);
+  setTimeout(move, 1500);
 }
 
 
 var instaWobble = function() {
   $("#insta").removeClass('slideOutLeft');
   $('#insta').addClass('animated wobble');
-  $('.feed2').removeClass('hide');
-  $('.feed2').animate({height:'85%'}, 300);
+  var f = $('.feed2');
+  f.removeClass('hide');
+  f.animate({left:'23%'}, {duration:300, queue: false});
+  f.animate({width:'74%'}, {duration:300, queue: false});
+  f.animate({bottom:'92%'}, {duration:300, queue: true});
+  f.animate({height:'85%'}, {duration:300, queue: false});
 }
 var instaMove = function() {
   $("#insta").addClass('animated slideOutLeft');
@@ -33,5 +64,5 @@ var initInsta = function() {
   setTimeout(instaMove, 1700);
 }
 
-$("#tweet").on('click', initTwit);
+$("#bird").on('click', initTwit);
 $("#insta").on('click', initInsta);
